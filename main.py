b72592c436328a4c3cf9a771c346d4ad2b16f086
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-05-30 11:45:47 +0200

    Refactor Kodi plugin for better clarity and structure

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ import xbmcgui
 import xbmcplugin
 import xbmcvfs
 
-# --- Nastavenia ---
+# --- Configuration (Kodi Engine) ---
 HANDLE = int(sys.argv[1])
 BASE_URL = sys.argv[0]
 
-# Jarko, zmenené na tvoj názov tv_free
+# Cesta k dátam doplnku tv_free, kam uložíme vygenerovaný playlist
 ADDON_DATA_PATH = xbmcvfs.translatePath("special://profile/addon_data/plugin.video.tv_free/")
 
 if not xbmcvfs.exists(ADDON_DATA_PATH):
     xbmcvfs.mkdir(ADDON_DATA_PATH)
 
 def add_directory_item(label, action, icon=None, is_folder=True, video_url=None, tvg_id=""):
+    """Vytvorí položku v menu Kodi a priradí jej EPG značku pre IPTV Simple Client."""
     query = {'action': action}
     if video_url:
         query['url'] = video_url
@@ -31,19 +32,20 @@ def add_directory_item(label, action, icon=None, is_folder=True, video_url=None,
     if not is_folder:
         list_item.setProperty('IsPlayable', 'true')
         list_item.setInfo('video', {'title': label})
+        # Tieto ID posielame do playlistu a Kodi ich spáruje s profi EPG v IPTV Simple Clientovi
         list_item.setProperty('tvg-id', tvg_id)
         list_item.setProperty('tvg-logo', icon)
 
     xbmcplugin.addDirectoryItem(handle=HANDLE, url=url, listitem=list_item, isFolder=is_folder)
 
-# --- DEFINÍCIA KANÁLOV ---
+# --- ZOZNAMY STANÍC (Štandardné EPG ID pre PVR klientov) ---
 CHANNELS_SK = [
-    ("TV JOJ", "https://img.joj.sk/logo.png", "JOJ.sk", "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8"),
+    ("TV JOJ", "https://yt3.googleusercontent.com/8rPXBoj2l1nhd9C-DCXF-s3tx0i_36GJzJcxeMyYvyPpPNakQsyc5DYc5d_QLDeI74ILkmFSJQ=s900-c-k-c0x00ffffff-no-rj", "JOJ.sk", "https://live.cdn.joj.sk/live/andromeda/joj-1080.m3u8"),
     ("JOJ Plus", "https://i.ibb.co/21Xx2nnd/joj-plus.png", "JOJPlus.sk", "https://live.cdn.joj.sk/live/andromeda/plus-1080.m3u8"),
     ("JOJ KRIMI", "https://img.telkac.zoznam.sk/data/images/channel/2026/03/04/image_new_137.thumb.png", "JOJKrimi.sk", "https://live.cdn.joj.sk/live/andromeda/wau-1080.m3u8"),
     ("JOJ 24", "https://img.joj.sk/38a52c95-84ce-4c04-b70a-2289a9fd1541", "JOJ24.sk", "https://live.cdn.joj.sk/live/andromeda/joj_news-1080.m3u8"),
     ("JOJ Šport", "https://img.joj.sk/rx660n/662097da-11c1-434a-a923-3e00cdcb81e7", "JOJSport.sk", "https://live.cdn.joj.sk/live/andromeda/joj_sport-1080.m3u8"),
-    ("Šport STVR", "https://lookaside.fbsbx.com/lookaside/crawler/media/?media_id=1451376403675507", "STV4.sk", "https://n16.stv.livebox.sk/stv-tv/_definst_/stv4-lq.smil/playlist.m3u8?auth=b64:X2FueV98MTc3Nzk5NDQwMHwyZjhmMWJiMmZjN2JjNGYwNmQ4NTE0YmJkNjAwM2I4ZTQyMjg0MWFh"),
+    ("JOJ Šport 2", "https://static.hnonline.sk/images/slike/2025/12/04/o_4878486_1024.png", "JOJSport2.sk", "https://live.cdn.joj.sk/live/andromeda/joj_sport2-1080.m3u8"),
     ("Jojko", "https://i.ibb.co/TxFWhc1J/jojko.png", "Jojko.sk", "https://live.cdn.joj.sk/live/andromeda/jojko-1080.m3u8"),
     ("JOJ Family", "https://i.ibb.co/hJgjKqpF/joj-family.png", "JOJFamily.sk", "https://live.cdn.joj.sk/live/andromeda/family-1080.m3u8"),
     ("JOJ Cinema", "http://www.mediaguru.cz/wp-content/uploads/2016/06/Joj-Cinema_akt.png", "JOJCinema.sk", "https://live.cdn.joj.sk/live/andromeda/cinema-1080.m3u8"),
@@ -56,18 +58,20 @@ CHANNELS_SK = [
     ("TV Nitrička", "https://www.satelitnatv.sk/wp-content/uploads/2013/04/nitricka.jpg", "TVNitricka.sk", "https://dash4.antik.sk/live/test_nitricka/playlist.m3u8"),
     ("TV9", "https://www.fotelka.tv/image/cache/catalog/Regionalne/TV9-240x234.jpg", "TV9.sk", "https://dash4.antik.sk/live/test_tv9/playlist.m3u8"),
     ("TV 8", "https://www.digislovakia.sk/wp-content/uploads/2023/04/TV8-logo-2-300x231.png", "TV8.sk", "http://109.74.145.11:1935/tv8/ngrp:tv8.stream_all/playlist.m3u8"),
-    ("Senzi TV", "https://static.wikia.nocookie.net/cstv/images/8/85/Senzi.png", "Senzi.sk", "https://lb.streaming.sk/senzi/stream/playlist.m3u8")
+    ("Senzi TV", "https://static.wikia.nocookie.net/cstv/images/8/85/Senzi.png", "Senzi.sk", "https://lb.streaming.sk/senzi/stream/playlist.m3u8"),
+    ("Flow TV", "https://www.flowtv.sk/wp-content/uploads/2021/04/logo_flow_tv_web.png", "FlowTV.sk", "https://app.viloud.tv/hls/channel/04e456809c83928443e59f0a2fce8610.m3u8")
 ]
 
 CHANNELS_CZ = [
     ("Minimax", "https://www.minimaxcz.tv/storage/images/cWiGhWyxj8fFnyWQZxEX.png", "Minimax.cz", "http://88.212.15.19/live/test_minimax/playlist.m3u8"),
     ("Óčko", "https://parasite.cz/wp-content/uploads/2013/02/ocko1.jpg", "Ocko.cz", "https://ocko-live-dash.ssl.cdn.cra.cz/cra_live2/ocko.stream.1.smil/playlist.m3u8"),
-    ("ČT 24", "https://pecka.tv/wp-content/uploads/2025/12/studio-ct24-400x600.jpg", "CT24.cz", "https://dash2.antik.sk/live/ct24_avc_25p/playlist.m3u8")
+    ("ČT 24", "https://pecka.tv/wp-content/uploads/2025/12/studio-ct24-400x600.jpg", "CT24.cz", "https://dash2.antik.sk/live/ct24_avc_25p/playlist.m3u8"),
+    ("ČT Sport", "https://www.itelka.sk/wp-content/uploads/2023/04/ct-sport.png", "CTSport.cz", "http://88.212.15.19/live/test_ctsport_25p/playlist.m3u8")
 ]
 
-# --- FUNKCIE PRE PLAYLIST ---
+# --- FUNKCIE PRE GENERÁCIU PLAYLISTU ---
 
-def setup_pvr_playlist():
+def generate_pvr_playlist():
     """Vygeneruje .m3u súbor pre IPTV Simple Client."""
     m3u_path = os.path.join(ADDON_DATA_PATH, "playlist.m3u")
     try:
@@ -76,50 +80,59 @@ def setup_pvr_playlist():
             all_channels = CHANNELS_SK + CHANNELS_CZ
             for name, logo, tid, url in all_channels:
                 f.write(f'#EXTINF:-1 tvg-id="{tid}" tvg-logo="{logo}",{name}\n{url}\n')
-        xbmcgui.Dialog().ok("Playlist Hotový", f"Súbor nájdeš tu:\n{m3u_path}")
+        xbmcgui.Dialog().ok("PVR Playlist", f"Playlist úspešne vytvorený!\nCesta: {m3u_path}\n\nTeraz túto lokálnu cestu nastav v IPTV Simple Client.")
     except Exception as e:
-        xbmcgui.Dialog().error("Chyba", str(e))
+        xbmcgui.Dialog().error("Chyba", f"Zlyhalo generovanie: {str(e)}")
 
-# --- MENU ---
+# --- MENU STRUKTÚRA ---
 
 def show_main_menu():
-    add_directory_item("Živé vysielanie", "live_menu", is_folder=True)
-    add_directory_item("Vytvoriť Playlist pre PVR", "set_pvr_playlist", is_folder=False)
+    """Hlavné menu doplnku."""
+    add_directory_item("Živé vysielania", "live_menu", is_folder=True)
+    add_directory_item("Nastaviť playlist do PVR IPTV Simple Client priamo z pluginu", "set_pvr_playlist", is_folder=False)
     xbmcplugin.endOfDirectory(HANDLE)
 
 def show_live_menu():
-    add_directory_item("Slovenské kanály", "list_sk", is_folder=True)
-    add_directory_item("České kanály", "list_cz", is_folder=True)
+    """Menu pod Živými vysielaniami."""
+    add_directory_item("Slovenské TV", "list_sk", is_folder=True)
+    add_directory_item("České TV", "list_cz", is_folder=True)
     xbmcplugin.endOfDirectory(HANDLE)
 
-def list_channels(channel_list):
-    for name, logo, tid, url in channel_list:
+def list_slovak_channels():
+    """Zoznam slovenských staníc."""
+    for name, logo, tid, url in CHANNELS_SK:
+        add_directory_item(name, "play", icon=logo, is_folder=False, video_url=url, tvg_id=tid)
+    xbmcplugin.endOfDirectory(HANDLE)
+
+def list_czech_channels():
+    """Zoznam českých staníc."""
+    for name, logo, tid, url in CHANNELS_CZ:
         add_directory_item(name, "play", icon=logo, is_folder=False, video_url=url, tvg_id=tid)
     xbmcplugin.endOfDirectory(HANDLE)
 
 def play_video(stream_url, title):
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    """Spustí video v Kodi prehrávači s potrebnými hlavičkami."""
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
     referer = "https://www.joj.sk/"
     final_url = f"{stream_url}|User-Agent={urllib.parse.quote(user_agent)}&Referer={urllib.parse.quote(referer)}"
-    
     list_item = xbmcgui.ListItem(path=final_url)
     list_item.setInfo('video', {'title': title})
     xbmcplugin.setResolvedUrl(HANDLE, True, list_item)
 
+# --- Router ---
 if __name__ == '__main__':
     params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
     action = params.get('action')
-    
     if action == 'live_menu':
         show_live_menu()
     elif action == 'list_sk':
-        list_channels(CHANNELS_SK)
+        list_slovak_channels()
     elif action == 'list_cz':
-        list_channels(CHANNELS_CZ)
-    elif action == 'set_pvr_playlist':
-        setup_pvr_playlist()
+        list_czech_channels()
     elif action == 'play':
         play_video(params.get('url'), params.get('title'))
+    elif action == 'set_pvr_playlist':
+        generate_pvr_playlist()
     else:
         show_main_menu()
-
+    
